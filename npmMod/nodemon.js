c34e47c4d283ad5dd89408823d4fa3a9a6bef5cb
dikshachauhan18 <dikshachauhan187@gmail.com>
2024-02-03 21:25:45 +0530

    nodejs

---
+++ npmMod/nodemon.js
@@ -0,0 +1,8 @@
+//step 1: npm install -g nodemon
+//check if already installed by :  nodemon -v
+
+//now run your anyfile using nodemon filename.js
+//now whenever u will commit any change and save it, it will  reflect on your terminal automatically.
+
+//to come out or exit the terminal, press cntrl+c
+
